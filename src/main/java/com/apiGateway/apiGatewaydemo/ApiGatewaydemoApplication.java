47f3bbbcbc6d2705b03226eef1fbb64c015940fb
Angel Vilches U <angel.vilches@usach.cl>
2022-11-06 11:16:32 -0300

    Gateway service

---
+++ src/main/java/com/apiGateway/apiGatewaydemo/ApiGatewaydemoApplication.java
@@ -0,0 +1,14 @@
+package com.apiGateway.apiGatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class ApiGatewaydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewaydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apiGateway/apiGatewaydemo/ApiGatewaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiGateway.apiGatewaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewaydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
